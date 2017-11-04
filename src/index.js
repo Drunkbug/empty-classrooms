3cee47a1d79d287db4049903e76a48c8d3fc5dd8
drunkbug <qiang.l@husky.neu.edu>
2017-11-04 18:01:31 -0400

    heroku setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRounter from './routers/AppRouter';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configStore from './store/configStore';
+import { addBuilding } from './actions/buildings';
+import { addClassroom } from './actions/classrooms';
+import getVisibleBuildings from './selectors/buildings';
 import './styles/styles.scss';
 
+const store = configStore();
+const buildingOne = store.dispatch(addBuilding('Ryder Hall'));
+store.dispatch(addBuilding('Doge Hall'));
+store.dispatch(addClassroom(buildingOne.building.id, '301'));
 
-ReactDOM.render(<AppRounter />, document.getElementById('app'));
+const state = store.getState();
+console.log(state);
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
 
 // login page -> Student/School 
 // Student -> empty classroom lists
